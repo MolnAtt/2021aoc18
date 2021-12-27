4583ed2a138b80aefe0dcadd9380cbf02300a24a
Molnár Attila <molnar.attila@szlgbp.hu>
2021-12-27 23:20:37 +0100

    Add project files.

---
+++ 2021aoc18/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021aoc18
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Szám.Parse("[1,2]").Diagnosztika();
+            Szám.Parse("[[[[[9,8],1],2],3],4]").Diagnosztika();
+            Szám.Parse("[7,[6,[5,[4,[3,2]]]]]").Diagnosztika();
+            Szám.Parse("[[6,[5,[4,[3,2]]]],1]").Diagnosztika();
+            Szám.Parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]").Diagnosztika();
+            Szám.Parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]").Diagnosztika();
+
+            Console.WriteLine("------------------------");
+            Szám a = Szám.Parse("[[[[4,3],4],4],[7,[[8,4],9]]]");
+            Szám b = Szám.Parse("[1, 1]");
+            Console.WriteLine($"{a} + {b}");
+            (a + b).Diagnosztika();
+            */
+            string lista = @"[1,1]
+[2,2]
+[3,3]
+[4,4]";
+            lista = @"[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]";
+            lista = @"[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]";
+            lista = @"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]";
+
+            // lista.Split('\n').Select(Szám.Parse).Sum().Diagnosztika();
+
+            /*
+            Szám.Parse("[[1,2],[[3,4],5]]").Diagnosztika();
+            Szám.Parse("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]").Diagnosztika();
+            Szám.Parse("[[[[1,1],[2,2]],[3,3]],[4,4]]").Diagnosztika();
+            Szám.Parse("[[[[3,0],[5,3]],[4,4]],[5,5]]").Diagnosztika();
+            Szám.Parse("[[[[5,0],[7,4]],[5,5]],[6,6]]").Diagnosztika();
+            Szám.Parse("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]").Diagnosztika();
+
+            */
+            lista = @"[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
+
+            // lista.Split('\n').Select(Szám.Parse).Sum().Diagnosztika();
+
+
+            List<Szám> számlista = lista.Split('\n').Select(Szám.Parse).ToList();
+            foreach (Szám item in számlista)
+            {
+                item.Diagnosztika();
+            }
+            Console.WriteLine("------------------------");
+            foreach (Szám item in számlista)
+            {
+                item.Diagnosztika();
+            }
+            
+            Console.WriteLine("--------------------");
+
+            foreach ((Szám, Szám) pár in számlista.Párok())
+            {
+                (Szám a, Szám b) = pár;
+                Szám ö = a + b;
+                Console.WriteLine($"{ö.Magnitúdó} :  {a}   +   {b}   =   {ö}");
+                //Console.WriteLine($"{a}   +   {b}   =   ");
+            }
+            Console.WriteLine("--------------------");
+            foreach (Szám item in számlista.Összegek().OrderBy(x=>x.Magnitúdó))
+            {
+                item.Diagnosztika();
+            }
+            /*
+             */
+
+           
+
+
+            /** /
+            System.IO.File.ReadAllText("input.txt").Split('\n').Select(Szám.Parse).Sum().Diagnosztika();
+            /*/
+            List<Szám> összegek = System.IO.File.ReadAllText("input.txt").Split('\n').Select(Szám.Parse).Összegek();
+
+            // Console.WriteLine(String.Join("\n",összegek.Select(szám=>szám.Magnitúdó).OrderBy(x=>x)));
+            foreach (var item in összegek.OrderBy(x => x.Magnitúdó))
+            {
+                item.Diagnosztika();
+            }
+            /**/
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ 2021aoc18/Szám.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2021aoc18
+{
+    class Szám
+    {
+        Szám bal;
+        Szám jobb;
+        Szám szülő;
+        int mélység;
+        int tartalom;
+        bool Levél { get => tartalom != -1; }
+
+
+
+        public Szám(int tartalom, Szám szülő=null) : this(null, null, szülő, Mélység_származtatása(szülő), tartalom) { }
+        public Szám((Szám, Szám) t, Szám szülő) : this(t.Item1, t.Item2, szülő, Mélység_származtatása(szülő), -1) { }
+        public Szám(Szám bal, Szám jobb, Szám szülő, int mélység, int tartalom)
+        {
+            this.bal = bal;
+            this.jobb = jobb;
+            this.szülő = szülő;
+            this.mélység = mélység;
+            this.tartalom = tartalom;
+
+            Óvatosan(Szülő_Update,bal);
+            Óvatosan(Szülő_Update,jobb);
+            Óvatosan(Mélység_Update,bal);
+            Óvatosan(Mélység_Update,jobb);
+        }
+        public static Szám Parse(string str) => Parse(str.Trim(), null);
+        public static Szám Parse(string str, Szám szülő) => Parse(str, str.Legkülső_vessző_helye(), szülő);
+        static Szám Parse(string str, int i, Szám szülő) => i == str.Length ? new Szám(int.Parse(str), szülő) : new Szám(str.Kettétörése(i).Számai(), szülő);
+        public static Szám Copy(Szám szám) => szám == null ? null : new Szám(Copy(szám.bal), Copy(szám.jobb), null, szám.mélység, szám.tartalom).Szülő_Teljes_Update();
+
+        Szám Szülő_Teljes_Update()
+        {
+            if (bal != null) // nem írható át Óvatosannal, mert az már végtelen ciklushoz vezet!
+            {
+                bal.szülő = this;
+                bal.Szülő_Teljes_Update();
+            }
+            if (jobb != null)
+            {
+                jobb.szülő = this;
+                jobb.Szülő_Teljes_Update();
+            }
+            return this;
+        }
+
+        public static Szám operator +(Szám a, Szám b) => new Szám(Copy(a), Copy(b), null, 0, -1).Egyszerűsít();
+        public static Szám operator +(Szám a, int b) => Copy(a) + new Szám(b);
+        public static Szám operator +(int a, Szám b) => new Szám(a) + Copy(b);
+
+        public static void Óvatosan(Action f, Szám szám) { if (szám != null) f(); }
+        public static void Óvatosan(Action<Szám> f, Szám szám) { if (szám != null) f(szám); }
+        public static void Óvatosan(Action<Szám, int> f, Szám szám, int x) { if (szám != null) f(szám,x); }
+        public static Szám Óvatosan(Func<Szám, Szám> f, Szám szám) => szám == null ? null : f(szám);
+
+        public override string ToString() => Levél ? tartalom.ToString() : $"[{bal},{jobb}]";
+        public string ToGraphViz() => Levél ? "" : bal.ToGraphViz() + jobb.ToGraphViz() + $"\"{this}\"->\"{bal}\";\n\"{this}\"->\"{jobb}\";\n";
+
+        public Szám Egyszerűsít()
+        {
+            // Console.Error.WriteLine($"Egyszerűsítés: {this}");
+            Szám egyszerű_pár = Legbalsó_levélszülő();
+            if (egyszerű_pár != null)
+                return egyszerű_pár.Robbant().Egyszerűsít();
+
+            Szám hagyományos_szám = Legbalsó_nagy_szám();
+            if (hagyományos_szám != null)
+                return hagyományos_szám.Szétesik().Egyszerűsít();
+
+            return Gyökér;
+        }
+
+
+        static int Mélység_származtatása(Szám szülő) => szülő == null ? 0 : szülő.mélység + 1;
+        void Mélység_Update()
+        {
+            mélység = Mélység_származtatása(szülő);
+            if (bal != null) // nem írható át Óvatosannal, mert az már végtelen ciklushoz vezet!
+                bal.Mélység_Update();
+            if (jobb != null)
+                jobb.Mélység_Update();
+        }
+
+        void Szülő_Update(Szám szám) => szám.szülő = this;
+        bool Gyerek { get => szülő != null; }
+        bool Balgyerek { get => Gyerek && szülő.bal == this; }
+        bool Jobbgyerek { get => Gyerek && szülő.jobb == this; }
+        Szám Gyökér { get => szülő == null ? this : szülő.Gyökér; }
+        int Magasság { get => Levél ? 0 : Math.Max(bal.Magasság, jobb.Magasság) + 1; }
+        public int Magnitúdó { get => Levél ? tartalom : 3 * bal.Magnitúdó + 2 * jobb.Magnitúdó; }
+        Szám Jobbszomszéd { get => Óvatosan(Legbalsó_levél,Óvatosan(Jobb,Jobbszomszéddal_közös_ős())); }
+        Szám Balszomszéd { get => Óvatosan(Legjobbsó_levél,Óvatosan(Bal,Balszomszéddal_közös_ős())); }
+        static Szám Jobb(Szám szám) => szám.jobb;
+        static Szám Bal(Szám szám) => szám.bal;
+
+        void Csere(Szám szám) { if (Balgyerek) szülő.bal = szám; else szülő.jobb = szám; }
+        Szám Szétesik() { Csere(Parse($"[{tartalom / 2},{tartalom / 2 + tartalom % 2}]", szülő)); return Gyökér; }
+        Szám Robbant()
+        {
+            Óvatosan(Tartalomnövelés, Balszomszéd, bal.tartalom);
+            Óvatosan(Tartalomnövelés, Jobbszomszéd, jobb.tartalom);
+            Csere(new Szám(0,szülő));
+            return Gyökér;
+        }
+        static void Tartalomnövelés(Szám target, int ezzel) { target.tartalom += ezzel; }
+        static Szám Legbalsó_levél(Szám szám) => szám.Levél ? szám : (szám.bal.Levél ? szám.bal : Legbalsó_levél(szám.bal));
+        static Szám Legjobbsó_levél(Szám szám) => szám.Levél ? szám : (szám.jobb.Levél ? szám.jobb : Legjobbsó_levél(szám.jobb));
+        Szám Balszomszéddal_közös_ős() => Balgyerek ? szülő.Balszomszéddal_közös_ős() : szülő;
+        Szám Jobbszomszéddal_közös_ős() => Jobbgyerek ? szülő.Jobbszomszéddal_közös_ős() : szülő;
+        public Szám Legbalsó_levélszülő() => Legbalsó_levélszülő(4);
+        //Szám Legbalsó_levélszülő(int m) => (bal.Levél && jobb.Levél) ? (mélység < m ? null : this) : (bal.Levél ? jobb.Legbalsó_levélszülő(m) : Ha_itt_nincs_levélszülő_irány_jobbra(m, bal.Legbalsó_levélszülő(m)));
+        Szám Legbalsó_levélszülő(int m) 
+        {
+            if (Levél)
+                return null;
+            if (bal.Levél && jobb.Levél)
+                return mélység < m ? null : this;
+            if (bal.Levél)
+                return jobb.Legbalsó_levélszülő(m);
+            return Ha_itt_nincs_levélszülő_irány_jobbra(m, bal.Legbalsó_levélszülő(m));
+        }
+        Szám Ha_itt_nincs_levélszülő_irány_jobbra(int m, Szám balresult) => balresult == null ? jobb.Legbalsó_levélszülő(m) : balresult;
+
+        public Szám Legbalsó_nagy_szám() => Legbalsó_nagy_szám(10);
+        Szám Legbalsó_nagy_szám(int n) => n <= tartalom ? this : (Levél ? null : Ha_itt_nincs_nagy_szám_jobbra_keresd(n, bal.Legbalsó_nagy_szám(n)));
+        /*
+        * /
+        Szám Legbalsó_nagy_szám(int n)
+        {
+            if (n <= tartalom)
+                return this;
+            if (Levél)
+                return null;
+            return Ha_itt_nincs_nagy_szám_jobbra_keresd(n,bal.Legbalsó_nagy_szám(n));
+        }
+        /*
+        */
+        Szám Ha_itt_nincs_nagy_szám_jobbra_keresd(int n, Szám balresult) => balresult == null ? jobb.Legbalsó_nagy_szám(n) : balresult;
+        /*
+        Szám Ha_balra_nincs_jobbra_keresd(int n, Szám balresult)
+        {
+            if (balresult == null)
+                return jobb.Legbalsó_nagy_szám(n);
+            return balresult;
+        }
+        */
+
+        public void Diagnosztika()
+        {
+            //Console.WriteLine(ToGraphViz());
+            // Console.WriteLine($"==== {this} ====");
+            //Console.WriteLine($"Legbalsó egyszerű párja: {Legbalsó_levélszülő()}");
+            // Console.WriteLine($"Legbalsó nagy száma: {Legbalsó_nagy_szám()}");
+            Console.WriteLine($"Magnitúdó: {Magnitúdó} -- {this}");
+        }
+
+        
+    }
+
+    static class Stringkiterjesztések
+    {
+        public static int Legkülső_vessző_helye(this string str)
+        {
+            int nyitott_zárójelek_száma = 0;
+            int i = 0;
+            while (i < str.Length && !(nyitott_zárójelek_száma == 1 && str[i] == ','))
+            {
+                if (str[i] == '[')
+                    nyitott_zárójelek_száma++;
+                else if (str[i] == ']')
+                    nyitott_zárójelek_száma--;
+                i++;
+            }
+            return i;
+        }
+        public static (string, string) Kettétörése(this string str, int i) => (str.Substring(1, i - 1), str.Substring(i + 1, str.Length - 1 - (i + 1)));
+        public static (Szám, Szám) Számai(this (string, string) p) => (Szám.Parse(p.Item1), Szám.Parse(p.Item2));
+        public static Szám Sum(this IEnumerable<Szám> lista)
+        {
+            Szám s = lista.First();
+            foreach (Szám szám in lista.Skip(1))
+                s += szám;
+            return s;
+        }
+
+        public static List<Szám> Összegek(this IEnumerable<Szám> lista)
+        {
+            List<Szám> result = new List<Szám>();
+            Szám[] tömb = lista.ToArray();
+            for (int i = 0; i < tömb.Length; i++)
+                for (int j = i + 1; j < tömb.Length; j++)
+                {
+                    Szám ij = tömb[i] + tömb[j];
+                    Szám ji = tömb[j] + tömb[i];
+                    Console.WriteLine($"Magnitúdó = {ij.Magnitúdó} --- {tömb[i]}   +   {tömb[j]}   =   {ij}");
+                    Console.WriteLine($"Magnitúdó = {ji.Magnitúdó} --- {tömb[j]}   +   {tömb[i]}  ===   {ji}");
+                    result.Add(ij);
+                    result.Add(ji);
+                }
+            return result;
+        }
+
+        public static List<(Szám, Szám)> Párok(this IEnumerable<Szám> lista)
+        {
+            List<(Szám, Szám)> result = new List<(Szám, Szám)>();
+            Szám[] tömb = lista.ToArray();
+            for (int i = 0; i < tömb.Length; i++)
+                for (int j = i + 1; j < tömb.Length; j++)
+                {
+                    result.Add((tömb[i], tömb[j]));
+                    result.Add((tömb[j], tömb[i]));
+                }
+            return result;
+        }
+    }
+}
